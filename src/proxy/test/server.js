616a825c70a06ecdc453111f212161ad6f029be6
Alex Oberhauser <alex.oberhauser@sigimera.org>
2015-04-03 00:58:54 +0200

    Testing proxy to blockchain handler inter component communication.

--- src/proxy/test/server.js
+++ src/proxy/test/server.js
@@ -1,32 +1,40 @@
 var server = require('../lib/server');
+var Util = require('../lib/util');
 
 var assert = require('assert'),
-    http = require('http');
+    http = require('http'),
+    zmq = require('zmq'),
+    sock = zmq.socket('pull');
 
-var http = require("http");
+require('dotenv').load();
 
 var options = {
-    host: "127.0.0.1",
-    port: 8080,
+    host: process.env.IP,
+    port: process.env.PORT,
     path: "http://www.example.org",
     headers: {
       Host: "www.example.org"
     }
 };
 
+var count = 1;
 
 describe('Proxy', function () {
   before(function () {
-    server.listen(8080, '127.0.0.1');
+    server.listen(process.env.PORT, process.env.IP);
+    sock.connect(process.env.NEW_BLOCKCHAIN_SOCKET);
   });
 
   it("access to 'www.example.org' via proxy should return 200", function (done) {
     http.get(options, function(res) {
-      if (res.statusCode === 200 ) done();
-      else {
-        throw "proxy request failed!"
-      }
+      sock.on('message', function(msg){
+        if (res.statusCode === 200 &&
+            'c87cda54e2aa4d6ba0f7d462cfa7dc74d2711733a60f1d85151c795f4521d90a' === JSON.parse(msg).magicno ) {
+          done();
+        }
+      });
     });
+
   });
 
   after(function () {

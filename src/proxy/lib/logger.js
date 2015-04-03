72222f343ac96533d3120ad90f1db3111e2c09ce
Alex Oberhauser <alex.oberhauser@sigimera.org>
2015-04-03 11:49:19 +0200

    Making proxy log level changable via env variable.

--- src/proxy/lib/logger.js
+++ src/proxy/lib/logger.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var bunyan = require('bunyan');
+var level = process.env.LOG_LEVEL || "info";
 module.exports = bunyan.createLogger({
   name: "blackgate-proxy",
-  level: "trace",
+  level: level,
   src: false
 });
